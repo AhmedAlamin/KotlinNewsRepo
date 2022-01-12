ebe3dade293e121b473678a5bdd23b1f87034a80
Ahmed Alamin <engahmedegypto@gmail.com>
2022-01-12 19:26:39 +0200

    Splash screen waiting API response

---
+++ app/src/main/java/com/ahmedoalamin/kotlinnews/ViewModelExample.kt
@@ -0,0 +1,20 @@
+package com.ahmedoalamin.kotlinnews
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class ViewModelExample : ViewModel() {
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading = _isLoading.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            delay(3000)
+            _isLoading.value = false
+        }
+    }
+}
